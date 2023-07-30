97e41498a8535f433639c74dc065bdbff1ac5489
Roizor <aidenjs9@icloud.com>
2023-07-30 13:12:55 -0500

    xfghbv

--- src/globalUtil.js
+++ src/globalUtil.js
@@ -1,5 +1,8 @@
+const cfg = require('../config.json');
+
 module.exports = {
 	log: (msg, sockObj={}) => {
+		if (!cfg.log) return;
 		if (sockObj.isCompanion) {
 			console.log('[Companion] > ' + msg);
 		} else {
