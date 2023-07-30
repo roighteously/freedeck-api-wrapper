71cc8d6b294afafad3faa04ce56cf03fab7a2ef9
Roizor <aidenjs9@icloud.com>
2023-07-30 00:38:04 -0500

    asdf 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { io } = require("socket.io-client");
 const socket = io("ws://localhost:5754");
 
+const config = require('../config.json');
 const { log } = require('./globalUtil');
 
 socket.on('connect', (dat) => {
@@ -13,4 +14,4 @@ socket.on("session_invalid", () => {
 	log("Session invalid: kicked out")
 });
 
-auth()
+auth(config.loginID, config.passwd);
