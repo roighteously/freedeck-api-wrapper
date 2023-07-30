f9011eadc147b0b4c50a6a91030e50e107b6303d
Roizor <aidenjs9@icloud.com>
2023-07-30 13:19:29 -0500

    dfghbcbnvjnmk,

--- src/client/auth.js
+++ src/client/auth.js
@@ -3,16 +3,20 @@ const { log } = require('../globalUtil');
 module.exports = (loginID, passwd, socket) => {
 	// We'll need to authenticate w/ FD.
 	socket.emit("c2sr_login", loginID);
+	socket.loginTries = 0;
 
 	socket.on("s2ca_login", (loc) => {
 		log("Our login ID is " + loginID);
-		log("Would've been taken to " + loc)
+		log("Redirect: " + loc)
 		socket.emit("c2sr_login_cont", loginID);
 	})
 
 	socket.on('user_ack_cont', (dat) => {
 		if (dat === 'session_expired') {
 			log('Not allowed to continue login');
+			log('Retrying..');
+			socket.emit("c2sr_login", loginID);
+			socket.loginTries++;
 		} else {
 			log("Password protected server; using configured password");
 			socket.emit('c2sd_login', passwd);
@@ -26,7 +30,8 @@ module.exports = (loginID, passwd, socket) => {
 	})
 
 	socket.on('s2ca_incorrect_pass', () => {
-		log('Incorrect password, exiting.');
-		process.exit(0);
+		log('Incorrect password, retrying. [Tries: ' + socket.loginTries + ' ]');
+		socket.emit('c2sd_login', passwd);
+		socket.loginTries++;
 	})
 }
