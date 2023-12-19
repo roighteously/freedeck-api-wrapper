b3c7f88de167d1d8c07cf0e15a67e7da9dbc7816
Aiden <aidenjs9@icloud.com>
2023-12-19 13:24:30 -0600

    Uploaded by script, no commit msg added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const path = require('path');
+
+try {
+	require('fs').readFileSync(path.resolve('./config.json'))
+} catch(err) {
+	console.log('config non existant. creating')
+	require('fs').copyFileSync(path.resolve('./config.json.default'), path.resolve('./config.json'))
+}
+
+
 const companionInit = require('./companion');
 const clientInit = require('./client');
 
